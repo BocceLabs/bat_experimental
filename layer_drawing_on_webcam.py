0a062fdc1610af01adea6e7992e60fddc1606333
David Hoffman <david.r.hoffman@ieee.org>
2023-05-14 10:10:20 -0500

    now strokes don't connect so you can draw multiple segments on the screen

--- layer_drawing_on_webcam.py
+++ layer_drawing_on_webcam.py
@@ -13,18 +13,17 @@ import numpy as np
 class DrawingWidget(Widget):
     def __init__(self, **kwargs):
         super(DrawingWidget, self).__init__(**kwargs)
-        self.points = []
+        self.strokes = []
 
     def on_touch_down(self, touch):
         with self.canvas:
             Color(1, 1, 1)
             touch.ud["line"] = Line(points=(touch.x, touch.y), width=5)
-            self.points.append((touch.x, touch.y))
+            self.strokes.append([(touch.x, touch.y)])
 
     def on_touch_move(self, touch):
         touch.ud["line"].points += (touch.x, touch.y)
-        self.points.append((touch.x, touch.y))
-
+        self.strokes[-1].append((touch.x, touch.y))
 
 class MainWidget(FloatLayout):
     def __init__(self, **kwargs):
@@ -55,16 +54,17 @@ class MainWidget(FloatLayout):
         self.texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='rgb')
         self.texture.blit_buffer(frame.tobytes(), colorfmt='rgb', bufferfmt='ubyte')
 
-        # Clear the canvas and draw the previous points and the new texture
+        # Draw the previous strokes and the new texture
         with self.drawing_widget.canvas:
             self.drawing_widget.canvas.clear()
             Color(1, 1, 1)
             Rectangle(texture=self.texture, pos=self.pos, size=self.size)
-            Color(1, 0, 0)
-            for i in range(1, len(self.drawing_widget.points)):
-                p1 = self.drawing_widget.points[i-1]
-                p2 = self.drawing_widget.points[i]
-                Line(points=(p1[0], p1[1], p2[0], p2[1]), width=5)
+            for stroke in self.drawing_widget.strokes:
+                points = []
+                for point in stroke:
+                    points.extend([point[0], point[1]])
+                Line(points=points, width=5)
+
 
 
 class MyApp(App):
